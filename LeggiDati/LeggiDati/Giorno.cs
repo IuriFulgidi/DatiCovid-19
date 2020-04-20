bd374c6036ce98f628160dfbebb90f36f62fb1f6
iurifulgidi <iurifulgidi@hotmail.com>
2020-04-20 09:16:18 +0200

    aggiornamento dati e cambio del tipo delle proprietà

--- LeggiDati/LeggiDati/Giorno.cs
+++ LeggiDati/LeggiDati/Giorno.cs
@@ -9,32 +9,32 @@ namespace LeggiDati
     public class Giorno
     {
         public string Data { get; set; }
-        public string RicoveratiConSintomi { get; set; }
-        public string TerapiaIntensiva { get; set; }
-        public string TotaleOspedalizzati { get; set; }
-        public string IsolamentoDomiciliare { get; set; }
-        public string TotalePositivi { get; set; }
-        public string VariazioneTotalePositivi { get; set; }
-        public string NuoviPositivi { get; set; }
-        public string DimessiGuariti { get; set; }
-        public string Deceduti { get; set; }
-        public string TotaleCasi { get; set; }
-        public string Tamponi { get; set; }
+        public int RicoveratiConSintomi { get; set; }
+        public int TerapiaIntensiva { get; set; }
+        public int TotaleOspedalizzati { get; set; }
+        public int IsolamentoDomiciliare { get; set; }
+        public int TotalePositivi { get; set; }
+        public int VariazioneTotalePositivi { get; set; }
+        public int NuoviPositivi { get; set; }
+        public int DimessiGuariti { get; set; }
+        public int Deceduti { get; set; }
+        public int TotaleCasi { get; set; }
+        public int Tamponi { get; set; }
 
         public Giorno( string data, string RicSin, string TerInt, string TotOsp, string IsoDoc, string TotPos, string VarTotPos, string NuoPos, string DimGua, string Dec,string TotCas, string Tam )
         {
             Data = data.Substring(0,10);
-            RicoveratiConSintomi = RicSin;
-            TerapiaIntensiva = TerInt;
-            TotaleOspedalizzati = TotOsp;
-            IsolamentoDomiciliare = IsoDoc;
-            TotalePositivi = TotPos;
-            VariazioneTotalePositivi = VarTotPos;
-            NuoviPositivi = NuoPos;
-            DimessiGuariti = DimGua;
-            Deceduti = Dec;
-            TotaleCasi = TotCas;
-            Tamponi = Tam;
+            RicoveratiConSintomi = Convert.ToInt32(RicSin);
+            TerapiaIntensiva = Convert.ToInt32(TerInt);
+            TotaleOspedalizzati = Convert.ToInt32(TotOsp);
+            IsolamentoDomiciliare = Convert.ToInt32(IsoDoc);
+            TotalePositivi = Convert.ToInt32(TotPos);
+            VariazioneTotalePositivi = Convert.ToInt32(VarTotPos);
+            NuoviPositivi = Convert.ToInt32(NuoPos);
+            DimessiGuariti = Convert.ToInt32(DimGua);
+            Deceduti = Convert.ToInt32(Dec);
+            TotaleCasi = Convert.ToInt32(TotCas);
+            Tamponi = Convert.ToInt32(Tam);
 
         }
 
